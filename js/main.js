fd90a211f684e22afcd3157737edd89ced686e96
tcdw <tcdw2011@gmail.com>
2018-12-07 22:08:18 +0800

    大体完成

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 (function() {
     'use strict';
+    var nodeTypeMap = {
+        1: 'ELEMENT_NODE',
+        2: 'ATTRIBUTE_NODE',
+        3: 'TEXT_NODE',
+        4: 'CDATA_SECTION_NODE',
+        5: 'ENTITY_REFERENCE_NODE',
+        6: 'ENTITY_NODE',
+        7: 'PROCESSING_INSTRUCTION_NODE',
+        8: 'COMMENT_NODE',
+        9: 'DOCUMENT_NODE',
+        10: 'DOCUMENT_TYPE_NODE',
+        11: 'DOCUMENT_FRAGMENT_NODE',
+        12: 'NOTATION_NODE',
+    };
     function dom2efml(e, ref) {
         if (ref === undefined) {
             ref = {};
@@ -13,20 +27,6 @@
             ignoreEmptyTextNode = true;
         }
         var element = e instanceof Element ? e : document.querySelector(e);
-        var nodeTypeMap = {
-            1: 'ELEMENT_NODE',
-            2: 'ATTRIBUTE_NODE',
-            3: 'TEXT_NODE',
-            4: 'CDATA_SECTION_NODE',
-            5: 'ENTITY_REFERENCE_NODE',
-            6: 'ENTITY_NODE',
-            7: 'PROCESSING_INSTRUCTION_NODE',
-            8: 'COMMENT_NODE',
-            9: 'DOCUMENT_NODE',
-            10: 'DOCUMENT_TYPE_NODE',
-            11: 'DOCUMENT_FRAGMENT_NODE',
-            12: 'NOTATION_NODE',
-        };
         var output = '';
         /**
          * @param {Element} element
@@ -62,16 +62,31 @@
         recursive(element);
         return output;
     }
+    function popup(level, text) {
+        var messageBox = document.getElementById('message-box');
+        messageBox.style.display = 'block';
+        messageBox.className = 'message ' + level;
+        var content = messageBox.querySelector('.content');
+        content.textContent = text;
+    }
     var userInput = document.getElementById('user-input');
     var userOutput = document.getElementById('user-output');
     var userConvert = document.getElementById('user-convert');
     userConvert.addEventListener('click', function act() {
-        var result = '';
-        var parser = new DOMParser();
-        var data = parser.parseFromString(userInput.value, 'text/html');
-        Array.prototype.forEach.call(data.body.childNodes, function loop(e) {
-            result += dom2efml(e);
-        })
-        userOutput.value = result;
+        try {
+            var result = '';
+            var parser = new DOMParser();
+            var data = parser.parseFromString(userInput.value, 'text/html');
+            Array.prototype.forEach.call(data.body.childNodes, function loop(e) {
+                if (e instanceof Element) {
+                    result += dom2efml(e);
+                } else {
+                    popup('warn', "There are non-element node(s) didn't handled by dom2efml");
+                }
+            });
+            userOutput.value = result;
+        } catch (e) {
+            popup('error', e);
+        }
     });
 })();
